0dd3c0dfa8218a03a5cbb7827090cf56f4470f51
Jordan Dent <jmandent@gmail.com>
2017-09-27 23:47:58 -0500

    Add files via upload

---
+++ If statement/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int x = 90;
+    if(x<=90){
+        cout << " You're Ugly" << endl;
+
+    }
+    return 0;
+}
---
+++ else statemt/if/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int age = 119;
+
+    if(age > 60)
+        { if(age > 100){
+            cout << "How are you alive??"<< endl;}
+        }
+    else{
+        cout << "You're young"<< endl;
+    }
+    return 0;
+}
